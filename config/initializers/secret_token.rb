0c379ffd6000e8762f5fffdee9fa50f118fd859e
Athenaa <Athenaa@telenav.com>
2013-03-26 19:37:34 -0700

    X-( git problemgit status

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '1bf91f4117babe2cce6c821fc827667e9878cce055e9bd6866b185f76b9511a37c40dc178c00cbea6c48628ffc3e2fbcf0278f67754adbf5c373366a4fd38a79'
